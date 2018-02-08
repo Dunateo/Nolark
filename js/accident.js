216e57374330963748621e445be83df292e939b3
Val <Val@172.16.204.181>
2018-02-08 13:26:50 +0100

    ajout de la doc des fonctions du formulaire

--- js/accident.js
+++ js/accident.js
@@ -4,17 +4,31 @@
  * and open the template in the editor.
  */
 
+/**
+ * LA fonction main 
+ * @return {Number}
+ */
+
 function calculprime(){
     const distance = 0.1;
     const ancien = 300;
     var nbdist,nbanc,nbaccid,maj,primed,primeanc,obtprime;
+    nbdist = 7;
+    nbaccid = 2;
+    nbanc = 1;
     primed =  primedistance(distance,nbdist);
     primeanc = anciennete(ancien,nbanc);
     obtprime = obtentionprime(nbaccid);
     maj = (primed+primeanc)/obtprime;
-    
-    
+    console.log(maj);
+    return 0;
 }
+/**
+ * calcul de la distance
+ * @param {number} distance
+ * @param {number} nbdist
+ * @return {Number}
+ */
 function primedistance(distance,nbdist){
     var maj;
     
@@ -24,8 +38,15 @@ function primedistance(distance,nbdist){
     else{
         maj = nbdist * distance;
     }
+    
     return maj;
 }
+/**
+ * Fonction qui calcul l'ancienneté
+ * @param {number} ancien
+ * @param {number} nbanc
+ * @return {Number}
+ */
 function anciennete(ancien,nbanc){
     var maj;
     if(nbanc<4){
@@ -39,8 +60,14 @@ function anciennete(ancien,nbanc){
             maj = 30 + ancien;
         }
     }
+
     return maj;
 }
+/**
+ * calcul des accidents
+ * @param {number} nbaccid
+ * @return {Number}
+ */
 function obtentionprime(nbaccid){
     var maj;
     if(nbaccid === 1){
@@ -55,8 +82,13 @@ function obtentionprime(nbaccid){
     if(nbaccid > 3){
         maj = 0;
     }
-    else{
+    if(nbaccid === 0){
         maj = 1;
     }
+    if(nbaccid <0){
+        maj = 1;
+    }
+     
     return maj;
 }
+window.addEventListener("load", calculprime(),false);
