3444f5bfd18ba3463c1a35a3378d2598e6bcbd40
Val <Val@172.16.204.181>
2018-03-15 11:32:45 +0100

    test unitaire en cours...

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+
 /**
 * Fonction qui retourne l'alcool pur ingéré en fonction du nombre de verre
 *
@@ -13,4 +14,12 @@
 function getAlcoolPur(nbVerres) {
  const uniteAlcool = 10;
  return uniteAlcool * nbVerres;
+}
+
+function getCoefDiffusion(sexe) {
+ if (sexe === 'homme') {
+ return 0.7;
+ } else {
+ return 0.6;
+ }
 }
