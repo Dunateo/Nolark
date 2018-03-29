3f12b55d99aa4b5ba1048d3495b14ac830f12fa5
Val <Val@172.16.204.181>
2018-03-29 11:01:46 +0200

    mise en place des tests

---
+++ test/unit/testRemuneration.js
@@ -0,0 +1,55 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+MesTestsUnitaires = TestCase('RemunerationTest');
+MesTestsUnitaires.prototype.testsRecupAnciennete = function () {
+    assertEquals('0 anciennete', 0.0, recupAnciennete(0, 1100.0));
+    assertEquals('5 anciennete', 33.0, recupAnciennete(5, 1100.0));
+    assertEquals('10 anciennete', 66.0, recupAnciennete(10, 1100.0));
+};
+MesTestsUnitaires.prototype.testsRecupS20 = function () {
+    assertEquals('0 casques', 0.0, recupComS20(0));
+    assertEquals('40 casques', 112.0, recupComS20(40));
+};
+MesTestsUnitaires.prototype.testsComXS = function () {
+    assertEquals('0 casques', 0.0, recupComXS(0));
+    assertEquals('50 casques', 0.0, recupComXS(50));
+    assertEquals('51 casques', 21.0, recupComXS(51));
+};
+MesTestsUnitaires.prototype.testsComMulti = function () {
+    assertEquals('0 casques', 0.0, recupComMulti(0));
+    assertEquals('20 casques', 144.0, recupComMulti(20));
+    assertEquals('50 casques', 468.0, recupComMulti(50));
+    assertEquals('51 casques', 486.0, recupComMulti(51));
+};
+MesTestsUnitaires.prototype.testsRecupIndemKm = function () {
+    assertEquals('en dessous de 350', 22.5, recupIndemKm(150));
+    assertEquals('au dessus de 350', 350.0, recupIndemKm(2500));
+};
+MesTestsUnitaires.prototype.testsAffRemuneration = function () {
+    /*:DOC +=
+     <fieldset id="resultats">
+     <h2 id="remuneration"></h2>
+     </fieldset>
+     */
+    assertTrue('Test aff remuneration', 'La rémunération sera de : 1100 €' === affRemuneration(1100));
+
+};
+MesTestsUnitaires.prototype.testsRecupValeur = function () {
+    /*:DOC +=
+     <fieldset id="resultats">
+     <legend>Vos résultats : </legend>
+     <p><label for="num_ancien">Nombre d'années d'ancienneté :</label> <input type="number" name="num_ancien" id="num_ancien" min="0" max="50" value="0" /></p>
+     <p><label for="num_s20">Nombre de S 20 vendus :</label> <input type="number" name="num_s20" id="num_s20" min="0" max="9999" value="0" /></p>
+     <p><label for="num_xspirit">Nombre de X-Spirit vendus :</label> <input type="number" name="num_xspirit" id="num_xspirit" min="0" max="9999" value="0" /></p>
+     <p><label for="num_multi">Nombre de Multitec vendus :</label> <input type="number" name="num_multi" id="num_multi" min="0" max="9999" value="0" /></p>
+     <p><label for="num_km">Distance parcourue :</label> <input type="number" name="num_km" id="num_km" min="0" max="9999" value="0" /></p>
+     </fieldset>
+     */
+    assertTrue('Test recup anc', '50' === recupValeur("#num_ancien input[type="number"]:50"););
+
+};
